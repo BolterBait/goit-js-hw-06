ea0cc416e59099f87a0eec528fa23454a8a572e7
Oleksandr Svyatobog <osvyatobog@gmail.com>
2022-08-07 12:22:05 +0300

    Task-3

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,18 @@
-// const listEl = document.querySelectorAll("ul#categories .item");
-// console.log("Number of categories:", listEl.length);
+const listEl = document.querySelectorAll("ul#categories .item");
+console.log("Number of categories:", listEl.length);
 
-// const titleEl = document.querySelectorAll("li.item");
+const titleEl = document.querySelectorAll("li.item");
 
-// titleEl.forEach((item) => {
-//   console.log(`Category:  ${item.querySelector("h2").textContent}`);
-//   console.log(`Elements: ${item.querySelectorAll("ul > li").length}`);
-// });
+titleEl.forEach((item) => {
+  console.log(`Category:  ${item.querySelector("h2").textContent}`);
+  console.log(`Elements: ${item.querySelectorAll("ul > li").length}`);
+});
 
-const categoriesList = document.querySelector("#categories");
-// console.log(categoriesList);
-const categoriesEl = Array.from(categoriesList.children);
-console.log(`Number of categorias: ${categoriesEl.length}`);
-categoriesEl.map((element) =>
-  console.log(`Category: ${element.querySelector("h2").textContent}
-  Elements: ${element.querySelectorAll("li").length}`)
-);
+// const categoriesList = document.querySelector("#categories");
+
+// const categoriesEl = Array.from(categoriesList.children);
+// console.log(`Number of categorias: ${categoriesEl.length}`);
+// categoriesEl.map((element) =>
+//   console.log(`Category: ${element.querySelector("h2").textContent}
+//   Elements: ${element.querySelectorAll("li").length}`)
+// );
