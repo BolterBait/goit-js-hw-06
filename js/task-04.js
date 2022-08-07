99e02179521ef03ee65593363f9807349745b78e
osvyatobog <osvyatobog@gmail.com>
2022-08-07 18:39:09 +0300

    Task4 finish

--- js/task-04.js
+++ js/task-04.js
@@ -1,17 +1,17 @@
 let counterValue = document.querySelector("#value");
 const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
-// counterValue = 0;
+let count = 0;
 console.log(decrementBtn);
 console.log(counterValue);
-
-// const icnreaseCounterByClick = function onClick() {
-//   counterValue += 1;
-// };
+counterValue.innerHTML = count;
 
 decrementBtn.addEventListener("click", () => {
-  counterValue += 1;
+  count -= 1;
+  counterValue.innerHTML = count;
 });
 
-// incrementBtn.addEventListener("counterValue", icnreaseCounterByClick);
-// console.log(counterValue);
+incrementBtn.addEventListener("click", () => {
+  count += 1;
+  counterValue.innerHTML = count;
+});
